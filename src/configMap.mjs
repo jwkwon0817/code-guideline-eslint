412e295526fa9ff0f2d7fa191ff17b245675be91
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-07-13 02:37:31 +0900

    chore: update version to 0.1.2 in package.json and refactor NestJS configuration in configMap.mjs and nestjs.mjs

--- src/configMap.mjs
+++ src/configMap.mjs
@@ -1,13 +1,16 @@
+import reactFormatterConfig from './react/formatter.mjs';
 import nestjsFormatterConfig from './nestjs/formatter.mjs';
 import nestjsImportConfig from './nestjs/import.mjs';
-import nestjsNestConfig from './nestjs/nestjs.mjs';
 import nestjsRestrictConfig from './nestjs/restrict.mjs';
-import reactFormatterConfig from './react/formatter.mjs';
+import nestjsNestConfig from './nestjs/nestjs.mjs';
 
 const configMap = {
   react: [reactFormatterConfig],
-  nestjs:  [
-    nestjsFormatterConfig, nestjsImportConfig, ...nestjsRestrictConfig, nestjsNestConfig,
+  nestjs: [
+    nestjsFormatterConfig,
+    nestjsImportConfig,
+    ...nestjsRestrictConfig,
+    ...nestjsNestConfig,
   ],
 };
 
--- src/nestjs/nestjs.mjs
+++ src/nestjs/nestjs.mjs
@@ -1,13 +1,3 @@
 import nestjsTyped from '@darraghor/eslint-plugin-nestjs-typed';
 
-/**
- * @type {import('eslint').Linter.Config}
- */
-const config = {
-  plugins: { '@darraghor/nestjs-typed': nestjsTyped },
-  rules:   {},
-
-  ...nestjsTyped.configs.flatRecommended,
-};
-
-export default config;
+export default nestjsTyped.configs.flatRecommended;
